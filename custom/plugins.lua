d136514a91d98172485a3af958efffece3bef24c
Aritra <chatterjee.aritra@protonmail.com>
2023-06-14 18:54:54 +0530

    Nvchad lspconfig

---
+++ custom/plugins.lua
@@ -0,0 +1,22 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "vscode-eslint-language-server",
+        "vscode-html-language-server",
+      },
+    },
+  }
+}
+
+return plugins
